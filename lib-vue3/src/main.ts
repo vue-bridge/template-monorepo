c74c9aac7fa9dd1d4d71766e64597c1ea07b0e90
Thorsten Luenborg <t.luenborg@googlemail.com>
2022-04-03 21:36:45 +0200

    fix: small TS fix in the example app.

--- lib-vue3/src/main.ts
+++ lib-vue3/src/main.ts
@@ -3,5 +3,5 @@ import type { App } from "@vue-bridge/runtime";
 export { MyInput };
 
 export function install(app: App) {
-  app.component(MyInput.name, MyInput);
+  app.component(MyInput.name!, MyInput);
 }
